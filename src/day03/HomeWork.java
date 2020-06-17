b340a8be8b86a149ddf6963233f3c99b9d206125
xiaozhongzhong2333 <2439021135@qq.com>
2020-06-17 08:47:02 +0800

    任众作业第一次项目结构上传

---
+++ src/day03/HomeWork.java
@@ -0,0 +1,15 @@
+package day03;
+
+public class HomeWork {
+    //打印九九乘法   等腰三角形
+    public static void main(String[] args) {
+        for(int i=1;i<=9;i++){
+            for (int j=1;j<=9;j++){
+                System.out.println(i*j+"="+i+"*"+j);
+            }
+            System.out.println("\n");
+
+        }
+    }
+
+}
