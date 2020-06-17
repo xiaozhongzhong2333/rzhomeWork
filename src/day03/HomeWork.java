64262052f37fccb4d63b6212b09b54b03db4fc1e
xiaozhongzhong2333 <2439021135@qq.com>
2020-06-17 09:00:45 +0800

    任众作业第一次项目结构上传

--- src/day03/HomeWork.java
+++ src/day03/HomeWork.java
@@ -2,14 +2,24 @@ package day03;
 
 public class HomeWork {
     //打印九九乘法   等腰三角形
-    public static void main(String[] args) {
+    public static void Judge(int side1,int side2,int side3){//判断等腰三角形
+        if((side1==side2||side2==side3)&&side1+side2>side3&&side1-side2<side3){
+            System.out.println("该三角形是等腰三角形");
+        }else{
+            System.out.println("该三角形不是等腰三角形");
+        }
+    }
+    public static  void Table(){//九九乘法表
         for(int i=1;i<=9;i++){
-            for (int j=1;j<=9;j++){
-                System.out.println(i*j+"="+i+"*"+j);
+            for (int j=1;j<=i;j++){
+                System.out.print(i+"*"+j+"="+i*j+" ");
             }
             System.out.println("\n");
-
         }
+    }
+    public static void main(String[] args) {
+        Table();
+        Judge(4,6,8);
     }
 
 }
